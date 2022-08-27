60b40aab4f67ff218d70a4c3d2fef41b43942c3d
赵坤禄 <1206570885@qq.com>
2022-08-27 18:01:23 +0800

    首页图片上传图床

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,24 +1,37 @@
-import { Component } from 'react'
-import { View, Text } from '@tarojs/components'
-import './index.scss'
+import { Component } from 'react';
+import { View, Text, Image } from '@tarojs/components';
+import './index.scss';
 
 export default class Index extends Component {
+  componentWillMount() {}
 
-  componentWillMount () { }
+  componentDidMount() {
+  }
 
-  componentDidMount () { }
+  componentWillUnmount() {}
 
-  componentWillUnmount () { }
+  componentDidShow() {}
 
-  componentDidShow () { }
+  componentDidHide() {}
 
-  componentDidHide () { }
-
-  render () {
+  render() {
     return (
-      <View className='index'>
-        <Text>首页</Text>
+      <View className='indexBox'>
+        <View className='indexMain'>
+          <View className='topBox'>
+            <View className='integral'>
+              <Image className='integralIcon' src='https://s3.bmp.ovh/imgs/2022/08/27/c9d844750dc2d666.png'></Image>
+              <Text className='integralText'>积分:300000</Text>
+            </View>
+          </View>
+          <View className='centerBox'></View>
+          <View className='bottomBox'>
+            <Image className='btnFirst' src='https://s3.bmp.ovh/imgs/2022/08/27/62a019db4b62bd98.png'></Image>
+            <Image className='btnSecond' src='https://s3.bmp.ovh/imgs/2022/08/27/2963de6239993932.png'></Image>
+            <Image className='btnThird' src='https://s3.bmp.ovh/imgs/2022/08/27/c4905cce2d2e73fb.png'></Image>
+          </View>
+        </View>
       </View>
-    )
+    );
   }
 }
